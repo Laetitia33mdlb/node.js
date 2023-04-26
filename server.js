ffe1acefcfdff06880e8018bd138fda0b6021971
Laetitia <laetitia.mdlb@gmail.com>
2023-04-26 21:59:34 +0200

    add variables

--- server.js
+++ server.js
@@ -1,2 +1,3 @@
-const sentence = "I am <name>, wilder in <city>, and I love <language>"
+require('dotenv').config()
+const sentence = `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`
 console.log(sentence)
